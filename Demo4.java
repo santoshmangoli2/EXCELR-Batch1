d3261ef46f02ca8b2a006dd2cf221f6267ce9b78
santoshmangoli2 <mangolisantosh2@gmail.com>
2025-01-04 12:20:18 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,12 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+            
+        }
+        
+       
+            
+        
+    }
+}
