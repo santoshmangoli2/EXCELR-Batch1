55250cd65960d0d6f23965fb4619138125dff35c
santoshmangoli2 <mangolisantosh2@gmail.com>
2025-01-21 11:30:29 +0530

    Add files via upload

---
+++ core java/Test003.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Test003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter item num: ");
+        int num = sc.nextInt();
+        System.out.println("Enter item name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter item quantity: ");
+        int quantity = sc.nextInt();
+
+        
+    }
+    
+}
