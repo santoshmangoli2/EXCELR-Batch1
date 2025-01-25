98024a1c4770a2ffb96441205a531da665940b3c
santoshmangoli2 <mangolisantosh2@gmail.com>
2025-01-25 11:26:51 +0530

    while loop

---
+++ Core.java/Demo5.java
@@ -0,0 +1,12 @@
+public class Demo5 {
+    
+    public static void main(String[] args) {
+        String name = "raju";
+        String pwd = "raju@123";
+        while ((name == "raju") && (pwd == "raju@123")){
+            System.out.println("hello wellcome");
+            break;
+
+        }
+    }
+}
