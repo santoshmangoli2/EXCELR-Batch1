c6e7f830bfb21da4bad2fbef4269301aabcd82e3
santoshmangoli2 <mangolisantosh2@gmail.com>
2025-01-25 11:27:17 +0530

    while loop

---
+++ Core.java/Demo6.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter u r name : ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter u r age : ");
+        int age = sc.nextInt();
+
+        System.out.println("enter u r phonenum: ");
+        long phonenum = sc.nextLong();
+
+            System.out.println("name : "+ name);
+            System.out.println("age : "+ age);
+            System.err.println("phonenum : "+ phonenum);
+        
+
+
+    }
+    
+}
